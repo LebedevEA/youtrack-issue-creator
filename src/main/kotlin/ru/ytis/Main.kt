35a48a1bc04f37f26742e9937def634ac529acd2
Lebedev Egor <egorikl@list.ru>
2021-09-19 22:19:00 +0300

    Remove useless information in Main.kt

--- src/main/kotlin/ru/ytis/Main.kt
+++ src/main/kotlin/ru/ytis/Main.kt
@@ -2,11 +2,6 @@ package ru.ytis
 
 import ru.ytis.rest.IssueClient
 
-object Main {
-    const val youTrackUrl = "http://lebedev:8080"
-    const val token = "perm:YWRtaW4=.NDUtMA==.sLddt8EYifIxJHzZ5rN6NjzLE4915R"
-}
-
 fun main() {
     try {
         val youTrackServer = Cli.readYouTrackServer()
